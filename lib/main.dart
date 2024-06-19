8e7aad573f351b40a27cf9f3731fdc16f235ed83
Anurag Thakur <97083108+DevAnuragT@users.noreply.github.com>
2024-06-19 17:51:05 +0530

    with assets

--- lib/main.dart
+++ lib/main.dart
@@ -1,10 +1,14 @@
 import 'package:flutter/material.dart';
 import 'package:firebase_core/firebase_core.dart';
 import 'package:campusbite/screens/welcome.dart';
+import 'package:flutter/services.dart';
 
 void main() async {
   WidgetsFlutterBinding.ensureInitialized();
   await Firebase.initializeApp();
+  SystemChrome.setPreferredOrientations([
+    DeviceOrientation.portraitUp,
+  ]);
   runApp(CampusBites());
 }
 
